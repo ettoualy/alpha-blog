8ee66529186e5785a617257046187c3c039b4afe
YOUNESS <younes.ettoualyy@gmail.com>
2020-05-31 17:28:19 +0100

    add pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,10 @@ class UsersController < ApplicationController
         @articles = @user.articles
     end
 
+    def index
+        @users = User.all
+    end
+
     def create
         @user = User.new(user_params)
         if @user.save
@@ -26,7 +30,7 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "Your account has successfully apdated"
-            redirect_to articles_path            
+            redirect_to @user            
         else
             render 'edit'
         end    
