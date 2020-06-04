3c14c8f52e160e83103866b8f328afdd033875f2
YOUNESS <younes.ettoualyy@gmail.com>
2020-06-04 22:59:13 +0100

    add admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
+    before_action :set_user , only: [:edit, :update, :show, :destroy]
+    before_action :require_user , only: [:edit, :update]
+    before_action :require_same_user , only: [:edit, :update, :destroy]
+
     def new
         @user = User.new
     end    
 
     def show
-        @user = User.find(params[:id])    
+
         @articles = @user.articles
     end
 
@@ -15,6 +19,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:notice] = "Welcome to the Alpha blog #{@user.username}, you have successfully sign up"  
             redirect_to articles_path
         else    
@@ -22,12 +27,19 @@ class UsersController < ApplicationController
         end
     end
 
+    def destroy
+        @user.destroy
+        session[:user_id] = nil if @user == current_user
+        flash[:notice] = "Account and all associated articles successfully deleted"
+        redirect_to root_path
+    end
+
     def edit
-        @user = User.find(params[:id])
+
     end
 
     def update
-        @user = User.find(params[:id])
+        
         if @user.update(user_params)
             flash[:notice] = "Your account has successfully apdated"
             redirect_to @user            
@@ -45,6 +57,18 @@ class UsersController < ApplicationController
     def user_params
         params.require(:user).permit(:username, :email, :password)
     end
+
+    def set_user
+        @user = User.find(params[:id])    
+    end
+
+    def require_same_user
+        if current_user != @user && !current_user.admin?
+          flash[:alert] = "You can only edit or delete your own account"
+          redirect_to @user
+        end  
+    end    
+    
     
     
     
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,10 @@ module ApplicationHelper
         image_src = "https://www.gravatar.com/avatar/#{hash}?s=#{options[:size]}"
         image_tag(image_src, alt: user.username, class: "rounded mx-auto d-block")   
     end
+
+
+
+
+    
+    
 end
